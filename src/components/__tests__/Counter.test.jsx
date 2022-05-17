cd657c1fe96ac0ab211f145028e5fd855b5427c3
chanyDev <chanyd.dev@gmail.com>
2022-05-17 23:43:06 +0900

    test : 카운터 컴포넌트 텍스트 확인 테스트 작성

--- src/components/__tests__/Counter.test.jsx
+++ src/components/__tests__/Counter.test.jsx
@@ -1,9 +1,13 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 
 import Counter from 'components/Counter';
 
-// test : 개별 테스트 수행
-test('Counter 컴포넌트를 렌더링합니다.', () => {
-  // render : 테스트를 위해 컴포넌트를 dom에 렌더링
-  render(<Counter />);
+describe('카운터 컴포넌트 테스트', () => {
+  test('Counter 컴포넌트에 count : 라는 텍스트가 보이는지 확인', () => {
+    render(<Counter />);
+
+    const countText = screen.getByText('count :');
+
+    expect(countText).toBeInTheDocument();
+  });
 });
